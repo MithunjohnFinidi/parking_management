812886a79b968ba86c58b5d62c34a42f86cfe928
MithunJohnFinidi <mithunjohn2@gmail.com>
2020-04-14 09:58:59 -0400

    procfile

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.set('port', process.env.PORT || 8080);
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -26,6 +27,8 @@ app.use('/users', usersRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
+app.listen(app.get('port'));
 
 // error handler
 app.use(function(err, req, res, next) {
