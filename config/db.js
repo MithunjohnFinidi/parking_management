34ed5ed9045f2a6582713167517f5c6ccdf4af0d
MithunJohnFinidi <mithunjohn2@gmail.com>
2020-04-14 22:09:18 -0400

    changed db

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 const Sequelize = require('sequelize');
 
-// module.exports = new Sequelize('heroku_a25aec331dd9283', 'ba4a3dec04d43d', '17b11576', {
-//     host: 'us-cdbr-iron-east-04.cleardb.net',
-//     dialect: 'mysql'
-// });
-
-module.exports = new Sequelize('capstone_test', 'root', '', {
-    host: 'localhost',
+module.exports = new Sequelize('heroku_a25aec331dd9283', 'ba4a3dec04d43d', '17b11576', {
+    host: 'us-cdbr-iron-east-04.cleardb.net',
     dialect: 'mysql'
 });
+
+// module.exports = new Sequelize('capstone_test', 'root', '', {
+//     host: 'localhost',
+//     dialect: 'mysql'
+// });
